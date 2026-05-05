a87a54ee3647c5edc125bdb6efc0bc7974214f0f
Himanshu gupta <himanshugpt0005@gmail.com>
2026-05-05 22:23:42 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -44,12 +44,12 @@ export const googleAuth = async (req, res) => {
 
     const token = await getToken(user._id);
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
+   res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,          // ✅ MUST be true in production (HTTPS)
+  sameSite: "None",      // ✅ MUST be None for cross-origin
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
     return res.status(200).json({ token });
 
